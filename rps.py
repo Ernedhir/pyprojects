be61a3b6e7f043e7d8f0c0b6975e93146252da60
Ernedhir <62746137+Ernedhir@users.noreply.github.com>
2025-07-14 15:24:57 +0300

    Added Rock Paper Scissors

---
+++ rps.py
@@ -0,0 +1,39 @@
+import time, random
+
+print("Initization finished!")
+moves = ["Rock", "Paper", "Scissor"]
+
+def init():
+    global selAI
+    selAI = random.choice(moves)
+    print(selAI)
+    time.sleep(1)
+
+def y():
+    print("You won!")
+def n():
+    print("AI won! :(")
+
+print("Welcome to the Rock Paper Scissors in Python!")
+time.sleep(2)
+
+while True:
+    init()
+    selME = input("Please select a move between 'Rock, Paper, Scissor' \n")
+    if selME not in moves:
+        print("Please select the correct keyword. Those are: (Rock, Paper, Scissor.)")
+    else: 
+        if selME == selAI: 
+            print ("Draw!")
+        elif selME == "Rock" and selAI == "Paper":
+            n()
+        elif selME == "Rock" and selAI == "Scissor":
+            y()
+        elif selME == "Paper" and selAI == "Rock":
+            y()
+        elif selME == "Paper" and selAI == "Scissor":
+            n()
+        elif selME == "Scissor" and selAI == "Paper":
+            y()
+        elif selME == "Scissor" and selAI == "Rock":
+            n()
