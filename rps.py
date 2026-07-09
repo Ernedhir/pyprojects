32d83514af0f7f19b6bc22a4b199e69ae45b77a3
Ernedhir <62746137+Ernedhir@users.noreply.github.com>
2026-07-09 04:24:12 +0300

    Rock Paper Scissor code is now a little bit more readable.

--- rps.py
+++ rps.py
@@ -1,6 +1,5 @@
 import time, random
 
-print("Initization finished!")
 moves = ["Rock", "Paper", "Scissor"]
 
 def init():
@@ -14,25 +13,23 @@ def n():
     print("AI won! :(")
 
 print("Welcome to the Rock Paper Scissors in Python!")
-time.sleep(2)
 
 while True:
     init()
     selME = input("Please select a move between 'Rock, Paper, Scissor' \n")
+    if selME == 'q': break
+
     if selME not in moves:
-        print("Please select the correct keyword. Those are: (Rock, Paper, Scissor.)")
+        print("Please select the correct move. Those are: (Rock, Paper, Scissor.)")
     else: 
         if selME == selAI: 
             print ("Draw!")
-        elif selME == "Rock" and selAI == "Paper":
-            n()
-        elif selME == "Rock" and selAI == "Scissor":
-            y()
-        elif selME == "Paper" and selAI == "Rock":
-            y()
-        elif selME == "Paper" and selAI == "Scissor":
-            n()
-        elif selME == "Scissor" and selAI == "Paper":
-            y()
-        elif selME == "Scissor" and selAI == "Rock":
-            n()
+        elif selME == 'Rock':
+            if selAI == 'Paper':     n()
+            elif selAI == 'Scissor': y()
+        elif selME == 'Paper':
+            if selAI == 'Scissor':   n()
+            elif selAI == 'Rock':    y()
+        elif selME == 'Scissor':
+            if selAI == 'Rock':      n()
+            elif selAI == 'Paper':   y()
